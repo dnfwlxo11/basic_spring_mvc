78e8af14175483a1c0b0c9cf124787ec214a28b0
dnfwlxo11 <dnfwlxo11@naver.com>
2021-10-04 23:06:19 +0900

    add: spring mvc logic and basic example

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+
+    @Bean
+    InternalResourceViewResolver internalResourceViewResolver() {
+        return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+    }
+}
